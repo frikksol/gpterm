8c5f0fc8af01c445e516050601450825f4d39b9c
Frikk Herding <frikksol@gmail.com>
2023-05-29 23:32:08 +0200

    🚀 Update release-script

--- release.sh
+++ release.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-VERSION=$1
+echo "✨ Creating new release"
 
 # Check that we are on main branch
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 if [[ "$BRANCH" != "main" ]]; then
-  echo 'Not on branch main!';
+  echo '🚨 Not on branch main! Aborting.';
   exit 1;
 fi
 
 # Verify that there are no active changes
 if [[ `git status --porcelain` ]]; then
-    echo 'There are changes in the repo! Please stash or commit them';
+    echo '🚨 There are changes in the repo! Please stash or commit them';
     exit 1;
 fi
 
+# Getting new version number
+echo "🔖 Current version is: $(cargo get version)"
+echo "Enter new version:"
+read NEW_VERSION
+echo "👌 Setting version to $NEW_VERSION"
+
+# Set new version and push
 git pull
-cargo v $VERSION
+cargo v $NEW_VERSION -y
 git push && git push --tags
